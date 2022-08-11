ab97d7e9294073b25d039b217be85a6bbc04a90e
robsonshockwave <robsonshockwavegithub@gmail.com>
2022-08-10 23:10:33 -0300

    feature: formatting the date of history item

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
@@ -27,7 +29,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutes} minutos</td>
-                  <td>{cycle.stardDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.stardDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
